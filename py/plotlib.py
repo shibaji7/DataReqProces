0cbe8541ec1f57e5444ba931a9d58f9d23744e44
shibaji7 <shibaji7@vt.edu>
2021-10-22 14:45:03 -0400

    Cartopy plots

---
+++ py/plotlib.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+"""plotlib.py: utility module for Costom Carto py geoaxes to plot data on aacgmv2 coordinates."""
+
+__author__ = "Chakraborty, S."
+__copyright__ = "Copyright 2020, SuperDARN@VT"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+plt.style.use(["science", "ieee"])
+
+import sys
+sys.path.extend(["py/"])
+
+import xarray
+import os
+import datetime as dt
+import argparse
+from dateutil import parser as prs
+import numpy as np
+
+import cartopy.crs as ccrs
+import cartopy
+import sdcarto
+
+def _add_colorbar(fig, ax, bounds, colormap, label=""):
+    """
+    Add a colorbar to the right of an axis.
+    """
+    import matplotlib as mpl
+    pos = ax.get_position()
+    cpos = [pos.x1 + 0.025, pos.y0 + 0.0125,
+            0.015, pos.height * 0.5]                # this list defines (left, bottom, width, height
+    cax = fig.add_axes(cpos)
+    norm = matplotlib.colors.BoundaryNorm(bounds, colormap.N)
+    cb2 = matplotlib.colorbar.ColorbarBase(cax, cmap=colormap,
+                                           norm=norm,
+                                           ticks=bounds,
+                                           spacing="uniform",
+                                           orientation="vertical")
+    cb2.set_label(label)
+    return
+
+def plot_map_grid_level_data(args):
+    print(f" Plot operations- {args.plot_type}")
+    ds = xarray.open_dataset(args.nc_file)
+    stime = [dt.datetime.utcfromtimestamp( (d - np.datetime64("1970-01-01T00:00:00Z","s")) / np.timedelta64(1, "s"))
+            for d in ds.coords["stime"].values]
+    etime = [dt.datetime.utcfromtimestamp( (d - np.datetime64("1970-01-01T00:00:00Z","s")) / np.timedelta64(1, "s"))
+            for d in ds.coords["stime"].values]
+    t_id = stime.index(args.sdate)
+    for p in args.plot_types:
+        geodetic = ccrs.Geodetic()
+        orthographic = ccrs.Orthographic(-90, 90)
+        fig = plt.figure(dpi=150, figsize=(3,3))
+        ax = fig.add_subplot(111, projection="sdcarto",\
+                             map_projection = orthographic,\
+                             coords="geo", plot_date=args.sdate)
+        ax.coastlines()
+        ax.add_dn_terminator()
+        ax.grid_on(draw_labels=[False, False, True, True])
+        if p == "pot":
+            lat, lon = ds.coords["lat_pot"].values, ds.coords["lon_pot"].values
+            pot = ds.data_vars["pot_arr"].values[t_id,:,:]
+            pot[pot==0]=np.nan
+            bounds = np.linspace(int(np.rint(np.nanmin(pot))/10)*10, int(np.rint(np.nanmax(pot))/10)*10, 11)
+            cmap = matplotlib.cm.get_cmap("Spectral")
+            cmap.set_bad("w", alpha=0.0)
+            norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+            XYZ = orthographic.transform_points(geodetic, lon, lat)
+            ax.contourf(XYZ[:,:,0], XYZ[:,:,1], pot, cmap=cmap, vmax=np.rint(np.nanmax(pot)), 
+                        vmin=np.rint(np.nanmin(pot)), transform=orthographic, alpha=0.8)
+            ax.set_global()
+            #ax.set_extent([-180,180,0,90], crs=orthographic)
+            _add_colorbar(fig, ax, bounds, cmap, r"Potential ($\Phi_{pc}$), Volts")
+            ax.text(0.01, 1.05, "Date: "+args.sdate.strftime("%Y-%m-%d")+"\n"+\
+                    "Time: %s UT"%(args.sdate.strftime("%H:%M")+"-"+(args.sdate+dt.timedelta(minutes=2)).strftime("%H:%M")),
+                    ha="left", va="center", transform=ax.transAxes)
+            ax.text(0.99, 1.05, "Coords: Geographic", ha="right", va="center", transform=ax.transAxes)
+        fig.savefig("tmp/%s-%s.%s.png"%(args.sdate.strftime("%Y%m%dT%H%M"), args.edate.strftime("%H%M"), p))
+        plt.close()
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-nc", "--nc_file", default="tmp/201110111200_201110111500_pot_north.nc", help="netCDF File name")
+    parser.add_argument("-s", "--sdate", default=dt.datetime(2011,10,11,12), help="Start date to plot", type=prs.parse)
+    parser.add_argument("-e", "--edate", default=dt.datetime(2011,10,11,15), help="End date to plot", type=prs.parse)
+    parser.add_argument("-p", "--plot_type", default="pot", help="Plot types", type=str)
+    args = parser.parse_args()
+    args.plot_types = args.plot_type.split(",")
+    for k in vars(args).keys():
+        print("     " + k + "->" + str(vars(args)[k]))
+    if "pot" in args.plot_types or "efield" in args.plot_types\
+            or "vel" in args.plot_types or "grid" in args.plot_types:
+        plot_map_grid_level_data(args)
+    else: print(f" Invalid plot operation {args.plot_type}!")
+    os.system("rm -rf .empty __pycache__")
--- py/sdcarto.py
+++ py/sdcarto.py
@@ -100,12 +100,24 @@ class SDCarto(GeoAxes):
         gl.yformatter = LATITUDE_FORMATTER
         return
     
+    def enum(self, bounds=[-120, -70, 25, 70], text_coord=False, dtype=None):
+        if bounds is not None: self.set_extent(bounds)
+        if text_coord: 
+            if self.coords == "geo": self.text(1.05, 0.7, "Geographic Coordinates", horizontalalignment="center",
+                                               verticalalignment="center", transform=self.transAxes, rotation=90)
+        self.text(0.1, 1.02, "Rad: "+self.rad, horizontalalignment="center",
+                verticalalignment="center", transform=self.transAxes)
+        if dtype is not None: self.text(1.03, 0.1, dtype, ha="center", va="center", transform=self.transAxes, rotation=90)
+        if self.plot_date: self.text(0.75, 1.02, self.plot_date.strftime("%Y-%m-%d %H:%M") + " UT", horizontalalignment="center",
+                verticalalignment="center", transform=self.transAxes)
+        return
+    
     def add_dn_terminator(self, **kwargs):
         """ Adding day night terminator """
         from cartopy.feature.nightshade import Nightshade
         if self.plot_date:
             ns_feature = Nightshade(self.plot_date, alpha=0.2)
-            super().add_feature(feature, **kwargs)
+            super().add_feature(ns_feature, **kwargs)
         return
     
     def get_aacgm_geom(self, feature, out_height=300. ):
@@ -118,22 +130,42 @@ class SDCarto(GeoAxes):
             for _ni, _list in enumerate(geo_coords):
                 mlon_check_jump_list = []
                 split_mag_list = None
-                if len(_list) == 1:
-                    _loop_list = _list[0]
-                else:
-                    _loop_list = _list
-                for _ngc, _gc in enumerate(_loop_list):
-                    _mc = aacgmv2.get_aacgm_coord(_gc[1], _gc[0], out_height, self.plot_date)
-                    if numpy.isnan(_mc[0]):
-                        continue 
+                if len(_list) == 2:
+                    _mc = aacgmv2.get_aacgm_coord(_list[1], _list[0], out_height, self.plot_date)
+                    if numpy.isnan(_mc[0]): continue
                     mlon_check_jump_list.append( _mc[1] )
-                    if self.coords == "aacgmv2":
-                        mag_list.append( (_mc[1], _mc[0]) )
+                    if self.coords == "aacgmv2": mag_list.append( (_mc[1], _mc[0]) )
                     else:
-                        if _mc[2]*15. > 180.:
-                            mag_list.append( (_mc[2]*15.-360., _mc[0]) )
+                        if _mc[2]*15. > 180.: mag_list.append( (_mc[2]*15.-360., _mc[0]) )
+                        else: mag_list.append( (_mc[2]*15., _mc[0]) )
+                elif len(_list) > 2:
+                    for _ngc, _gc in enumerate(_list):
+                        _mc = aacgmv2.get_aacgm_coord(_gc[1], _gc[0], out_height, self.plot_date)
+                        if numpy.isnan(_mc[0]): continue
+                        mlon_check_jump_list.append( _mc[1] )
+                        if self.coords == "aacgmv2": mag_list.append( (_mc[1], _mc[0]) )
                         else:
-                            mag_list.append( (_mc[2]*15., _mc[0]) )
+                            if _mc[2]*15. > 180.: mag_list.append( (_mc[2]*15.-360., _mc[0]) )
+                            else: mag_list.append( (_mc[2]*15., _mc[0]) )
+                #print(_list)
+                #if len(_list) == 1:
+                #    _loop_list = _list[0]
+                #else:
+                #    _loop_list = _list
+                #for _ngc, _gc in enumerate(_loop_list):
+                #    print(_loop_list, _ngc, _gc)
+                #    _mc = aacgmv2.get_aacgm_coord(_gc[1], _gc[0], out_height, self.plot_date)
+                #    if numpy.isnan(_mc[0]):
+                #        continue 
+                #    mlon_check_jump_list.append( _mc[1] )
+                #    if self.coords == "aacgmv2":
+                #        mag_list.append( (_mc[1], _mc[0]) )
+                #    else:
+                #        if _mc[2]*15. > 180.:
+                #            mag_list.append( (_mc[2]*15.-360., _mc[0]) )
+                #        else:
+                #            mag_list.append( (_mc[2]*15., _mc[0]) )
+                
                 # check for unwanted jumps
                 mlon_check_jump_list = numpy.array( mlon_check_jump_list )
 
